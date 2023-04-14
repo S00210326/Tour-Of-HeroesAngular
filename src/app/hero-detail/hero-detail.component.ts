d435eace1aa707ade4cceabc021c9e2f268d46d3
S00210326 <s00210326@atu.ie>
2023-04-14 09:01:37 +0200

    first commit

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { MessageService } from '../message.service';
+import { HeroService } from '../hero.service';
+import { Observable, of } from 'rxjs';
+import { HEROES } from '../mock-heroes';
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent {
+  @Input() hero?: Hero;
+
+  constructor(private route:ActivatedRoute, private heroService:HeroService, private location:Location, 
+    private messageService:MessageService) {
+    
+  }
+  ngOnInit():void{
+    this.getHero();
+  }
+  getHero():void{
+    const id = 
+    Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+    .subscribe(hero => this.hero = hero)
+  }
+  goBack(): void {
+    this.location.back();
+  }
+  save():void{
+    if(this.hero){
+      this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+    }
+  }
+
+  
+}
